1b868aa490a82d52f88e4179c2715b8bc726bfdb
n111ko <niko.aoapp@gmail.com>
2025-02-06 10:20:08 +0200

    Update protseduurid.sql

--- protseduurid.sql
+++ protseduurid.sql
@@ -1,2 +1,112 @@
 -- SQL SALVESTATUD PROTSEDUURID -- Funktsioonid - mitu SQL käsku käivitakse järjest
 -- SQL SERVER 
+
+CREATE DATABASE procTARgv24;
+
+USE procTARgv24;
+
+CREATE TABLE uudised(
+uudisID int PRIMARY KEY identity(1,1),
+uudiseTeema varchar(50),
+kuupaev date,
+autor varchar(25),
+kirjeldus text
+)
+
+SELECT * FROM uudised;
+
+INSERT INTO uudised(uudiseTeema, kuupaev, autor, kirjeldus)
+VALUES(
+'udune ilm', '2025-02-06', 'postimees', 'Lõunani on udune ilm')
+
+-- protseduuri loomine
+-- sisestab uudised tabelisse ja kohe näitab
+CREATE PROCEDURE lisaUudis
+@uusTeema varchar(50),
+@paev date,
+@autor varchar(20),
+@kirjeldus text
+AS
+BEGIN
+
+INSERT INTO uudised(uudiseTeema, kuupaev, autor, kirjeldus)
+VALUES(
+@uusTeema, @paev, @autor, @kirjeldus);
+SELECT * FROM uudised;
+
+END;
+
+--kutse
+EXEC lisaUudis 'windows 11', '2025-02-06', 'õpetaja Pant', 'w11 ei tööta multimeedia klassis';
+
+--teine kutse võimalus
+EXEC lisaUudis
+@uusTeema='1.märts on jub kevad',
+@paev='2025-02-06',
+@autor='test',
+@kirjeldus='puudub';
+
+-- protseduur, mis kustutab tabelist id järgi
+CREATE PROCEDURE kustutaUudis
+@id int
+AS
+BEGIN
+SELECT * FROM uudised;
+DELETE FROM uudised WHERE uudisID=@id;	
+SELECT * FROM uudised;
+END;
+
+-- kutse
+EXEC kustutaUudis 3;
+EXEC kustutaUudis @id=3;
+
+UPDATE uudised SET kirjeldus='uus kirjeldus'
+WHERE kirjeldus Like 'puudub';
+SELECT * FROM uudised;
+
+-- protseduur, mis uuendab andmed tabelis /UPDATE
+CREATE PROCEDURE uuendaKirjeldus
+@uuskirjeldus text
+AS
+BEGIN
+SELECT * FROM uudised;
+UPDATE uudised SET kirjeldus=@uuskirjeldus
+WHERE kirjeldus Like 'puudub';
+SELECT * FROM uudised;
+END;
+
+-- kutse
+EXEC uuendaKirjeldus 'uus tekst kirjelduses';
+
+-- protseduur, mis otsib ja näitab uudist esimese tähte järgi
+CREATE PROCEDURE otsingUudiseTeema
+@taht char(1)
+AS
+BEGIN
+SELECT * FROM uudised
+WHERE uudiseTeema LIKE @taht +'%';
+END;
+
+-- kutse
+EXEC otsingUudiseTeema 'w';
+
+---------------------------------------------------------------------------------
+-- XAMPP / localhost
+
+CREATE TABLE uudised(
+uudisID int PRIMARY KEY AUTO_INCREMENT,
+uudiseTeema varchar(50),
+kuupaev date,
+autor varchar(25),
+kirjeldus text
+);
+
+INSERT INTO uudised(uudiseTeema, kuupaev, autor, kirjeldus)
+VALUES(
+'udune ilm', '2025-02-06', 'postimees', 'Lõunani on udune ilm')
+
+--protseduurite screenid on read.me failis
+
+
+
+
